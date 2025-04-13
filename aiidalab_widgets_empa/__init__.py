683e3d53c82a25932837ac6bfd198f70640fd5e6
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2025-04-13 21:09:29 +0000

    Implement CdxmlUploadWidget

--- aiidalab_widgets_empa/__init__.py
+++ aiidalab_widgets_empa/__init__.py
@@ -1,7 +1,9 @@
 from .computational_resources import ProcessResourcesWidget, ResourcesEstimatorWidget
+from .cdxml import CdxmlUploadWidget
 from .version import __version__
 
 __all__ = [
+    "CdxmlUploadWidget",
     "ProcessResourcesWidget",
     "ResourcesEstimatorWidget",
     "__version__",
