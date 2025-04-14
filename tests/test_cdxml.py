ad2a4439df1168a63e0352ca84dc269fda970ef1
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2025-04-14 11:05:58 +0000

    added test for finite unit n=2  and for benzene

--- tests/test_cdxml.py
+++ tests/test_cdxml.py
@@ -30,4 +30,37 @@ def test_structure_upload_widget():
     widget.create_button.click()
     assert isinstance(widget.structure, ase.Atoms)
     assert widget.structure.get_chemical_formula() == "C14H4"
-    # assert np.all(widget.structure[0].position == [0, 0, 0])
+    # Simulate the structure upload.
+    widget._on_file_upload(
+        change={
+            "new": {
+                "7AGNR.cdxml": {
+                    "content": content,
+                }
+            }
+        }
+    )    
+    # sets 2 units, finite size
+    widget.nunits.value = '2'
+    widget.create_button.click()
+    assert isinstance(widget.structure, ase.Atoms)
+    assert widget.structure.get_chemical_formula() == "C56H22"
+    
+    # case of benzene molecule
+    filename = Path(__file__).parent / "benzene.cdxml"
+
+    with open(filename, "rb") as f:
+        content = f.read()
+    # Simulate the structure upload.
+    widget._on_file_upload(
+        change={
+            "new": {
+                "benzene.cdxml": {
+                    "content": content,
+                }
+            }
+        }
+    )    
+    widget.create_button.click()
+    assert isinstance(widget.structure, ase.Atoms)
+    assert widget.structure.get_chemical_formula() == "C6H6"
